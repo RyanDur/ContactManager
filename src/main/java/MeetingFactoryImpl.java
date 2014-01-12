17e9393dcdf245cfb39c1ae68648c90c016339d5
RyanDur <ryan.p.durling@gmail.com>
2014-01-12 23:56:53 +0000

    meeting factory

--- src/main/java/MeetingFactoryImpl.java
+++ src/main/java/MeetingFactoryImpl.java
@@ -4,4 +4,9 @@ public class MeetingFactoryImpl implements MeetingFactory {
     public static Meeting create(int id, Set<Contact> contacts, Calendar date) throws InvalidMeetingException {
 	return new Meeting(int id, Set<Contact> contacts, Calendar date);
     }
+
+    @Override
+    public static PastMeeting create(Meeting meeting, String notes) throws InvalidMeetingException {
+	return new PastMeeting(Meeting meeting, String notes);
+    }
 }
