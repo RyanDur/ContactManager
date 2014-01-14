0ffbe4ea2fa8a568cffd4892d31c9c33a1bb6c2c
RyanDur <ryan.p.durling@gmail.com>
2014-01-14 00:36:18 +0000

    Future meeting

---
+++ src/main/java/FutureMeetingImpl.java
@@ -0,0 +1,9 @@
+import java.util.Calendar;
+import java.util.Set;
+
+public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting {
+
+    public FutureMeetingImpl(int id, Set<Contact> contacts, Calendar date) throws InvalidMeetingException {
+	super(id, contacts, date);
+    }
+}
