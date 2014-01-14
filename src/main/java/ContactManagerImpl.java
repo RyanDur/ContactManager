946b7e71f24bd462567cf4903999bb36ee43fd32
RyanDur <ryan.p.durling@gmail.com>
2014-01-14 00:41:35 +0000

    the ability to have future meetings, need to catch the exception for unknown contact.

---
+++ src/main/java/ContactManagerImpl.java
@@ -0,0 +1,106 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Set;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.LinkedHashMap;
+
+public class ContactManagerImpl implements ContactManager {
+    private MeetingFactory meetingFactory;
+    private ContactFactory contactFactory;
+    private Map<Integer, Meeting> meetings;
+    private Map<Integer, FutureMeeting> futureMeetings;
+    private Map<Integer, PastMeeting> pastMeetings;
+    private Map<String, Contact> contactsByName;
+    private Map<Integer, Contact> contactsById;
+    private int meetingId;
+    private int contactId;
+
+    public ContactManagerImpl(MeetingFactory meetingFactory, ContactFactory contactFactory) {
+	this.contactFactory = contactFactory;
+        this.meetingFactory = meetingFactory;
+        meetings = new LinkedHashMap<Integer, Meeting>();
+        futureMeetings = new LinkedHashMap<Integer, FutureMeeting>();
+        pastMeetings = new LinkedHashMap<Integer, PastMeeting>();
+        contactsByName = new LinkedHashMap<String, Contact>();
+	contactsById = new LinkedHashMap<Integer, Contact>();
+        meetingId = 0;
+        contactId = 0;
+    }
+
+    @Override
+    public int addFutureMeeting(Set<Contact> contacts, Calendar date) throws IllegalArgumentException {
+        if(date.compareTo(new GregorianCalendar()) < 0) throw new IllegalArgumentException();
+        try {
+            FutureMeeting futureMeeting = meetingFactory.createFutureMeeting(meetingId, contacts, date);
+            futureMeetings.put(meetingId, futureMeeting);
+        }
+        catch (InvalidMeetingException e) {
+            System.out.println("Error " + e.getMessage());
+            e.printStackTrace();
+        }
+        return meetingId++;
+    }
+
+    @Override
+    public PastMeeting getPastMeeting(int id) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public FutureMeeting getFutureMeeting(int id) throws IllegalArgumentException {
+        FutureMeeting meeting = futureMeetings.get(id);
+        if(meeting.getDate().compareTo(new GregorianCalendar()) < 0) throw new IllegalArgumentException();
+        return meeting;
+    }
+
+    @Override
+    public Meeting getMeeting(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Meeting> getFutureMeetingList(Contact contact) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public List<Meeting> getFutureMeetingList(Calendar date) {
+        return null;
+    }
+
+    @Override
+    public List<PastMeeting> getPastMeetingList(Contact contact) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public void addNewPastMeeting(Set<Contact> contacts, Calendar date, String text) throws IllegalArgumentException, NullPointerException {
+
+    }
+
+    @Override
+    public void addMeetingNotes(int id, String text) throws IllegalArgumentException, IllegalStateException, NullPointerException {
+
+    }
+
+    @Override
+    public void addNewContact(String name, String notes) throws NullPointerException {
+
+    }
+
+    @Override
+    public Set<Contact> getContacts(int... ids) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public Set<Contact> getContacts(String name) throws NullPointerException {
+        return null;
+    }
+
+    @Override
+    public void flush() {}
+}
--- src/test/java/ContactManagerTest.java
+++ src/test/java/ContactManagerTest.java
@@ -1,8 +1,94 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Set;
+import java.util.HashSet;
+
 import java.util.*;
 import org.junit.*;
+import org.junit.rules.ExpectedException;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class ContactManagerTest {
+    private ContactManager cm;
+    private Set<Contact> mockContacts;
+    private Contact mockContact;
+    private Meeting mockMeeting;
+    private FutureMeeting mockFutureMeeting;
+    private PastMeeting mockPastMeeting;
+    private Calendar mockDate = mockDate(1);
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    private Set<Contact> getMockContacts(int numOfMocks) {
+        Set<Contact> set = new HashSet<Contact>();
+        for(int i = 0; i < numOfMocks; i++) {
+            set.add(mock(Contact.class));
+        }
+        assertEquals(numOfMocks, set.size());
+        return set;
+    }
+
+    @Before
+    public void setup() throws InvalidMeetingException {
+        mockContacts = new HashSet<Contact>();
+        mockContact = mock(Contact.class);
+        mockContacts.add(mockContact);
+        mockDate = mockDate(1);
+
+        mockMeeting = mock(Meeting.class);
+        mockFutureMeeting = mock(FutureMeeting.class);
+        mockPastMeeting = mock(PastMeeting.class);
+
+        cm = new ContactManagerImpl(mockMeetingFactory(), mockContactFactory());
+    }
+
+    @Test
+    public void shouldBeAbleToAddaMeetingForTheFuture() {
+        when(mockFutureMeeting.getDate()).thenReturn(mockDate);
+        int id = cm.addFutureMeeting(mockContacts, mockDate);
+        Meeting meeting = cm.getFutureMeeting(id);
+
+        assertThat(meeting, is(instanceOf(FutureMeeting.class)));
+        assertThat(mockFutureMeeting, is(equalTo(meeting)));
+        assertThat(meeting.getDate(), is(equalTo(mockDate)));
+    }
+
+    @Test
+    public void shouldThrowIllegalArgumentExceptionIfUserTyToAddPastMeetingToFutureDate() {
+        thrown.expect(IllegalArgumentException.class);
+
+        cm.addFutureMeeting(mockContacts, mockDate(-1));
+    }
+
+    @Test
+    public void shouldThrowIllegalArgumentExceptionIfUserTyToGetPastMeetingFromFutureMeeting() {
+        thrown.expect(IllegalArgumentException.class);
+        when(mockFutureMeeting.getDate()).thenReturn(mockDate(-1));
+        int id = cm.addFutureMeeting(mockContacts, mockDate);
+
+        cm.getFutureMeeting(id);
+    }
+
+    private Calendar mockDate(int days) {
+        Calendar date = new GregorianCalendar();
+        date.add(Calendar.DATE, days);
+        return date;
+    }
+
+    private MeetingFactory mockMeetingFactory() throws InvalidMeetingException {
+        MeetingFactory mockMeetingFactory = mock(MeetingFactory.class);
+        when(mockMeetingFactory.createMeeting(anyInt(), eq(mockContacts), eq(mockDate))).thenReturn(mockMeeting);
+        when(mockMeetingFactory.createFutureMeeting(anyInt(), eq(mockContacts), eq(mockDate))).thenReturn(mockFutureMeeting);
+        when(mockMeetingFactory.createPastMeeting(eq(mockMeeting), anyString())).thenReturn(mockPastMeeting);
+        return mockMeetingFactory;
+    }
+
+    private ContactFactory mockContactFactory() {
+        ContactFactory mockContactFactory = mock(ContactFactory.class);
+        when(mockContactFactory.createContact(anyInt(), anyString())).thenReturn(mockContact);
+        return mockContactFactory;
+    }
 }
