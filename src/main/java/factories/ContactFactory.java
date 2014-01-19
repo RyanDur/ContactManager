617d629e327c42264acc910a0fb594ddbfb193a8
RyanDur <ryan.p.durling@gmail.com>
2014-01-19 16:17:30 +0000

    Javadoc for the Contact factory

--- src/main/java/factories/ContactFactory.java
+++ src/main/java/factories/ContactFactory.java
@@ -5,9 +5,11 @@ import models.Contact;
 public interface ContactFactory {
 
   /**
-   * @param id
-   * @param name
-   * @return
+   *  create an instance of a Contact
+   *
+   * @param id     id of the contact
+   * @param name   name of the contact
+   * @return a new instance of a contact
    */
   Contact createContact(int id, String name);
 }
