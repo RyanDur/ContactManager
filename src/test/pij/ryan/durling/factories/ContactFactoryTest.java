eef54e82e5bbc1e8153c7231e8e2ab131d4cfabd
RyanDur <ryan.p.durling@gmail.com>
2014-02-04 15:57:45 +0000

    factory tests

---
+++ src/test/pij/ryan/durling/factories/ContactFactoryTest.java
@@ -0,0 +1,22 @@
+package pij.ryan.durling.factories;
+
+import org.junit.Test;
+import pij.ryan.durling.models.Contact;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.mockito.Mockito.*;
+
+public class ContactFactoryTest {
+
+  @Test
+  public void shouldGetAnInstanceOfAContact() {
+    ContactFactory cf = spy(ContactFactoryImpl.getInstance());
+    Contact expected = mock(Contact.class);
+    doReturn(expected).when(cf).createContact(anyInt(), anyString());
+    Contact actual = cf.createContact(0, "Ryan");
+
+    assertThat(actual, is(equalTo(expected)));
+  }
+}
---
+++ src/test/pij/ryan/durling/factories/MeetingFactoryTest.java
@@ -0,0 +1,47 @@
+package pij.ryan.durling.factories;
+
+import org.junit.Before;
+import org.junit.Test;
+import pij.ryan.durling.exceptions.InvalidMeetingException;
+import pij.ryan.durling.models.Contact;
+import pij.ryan.durling.models.FutureMeeting;
+import pij.ryan.durling.models.PastMeeting;
+
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.*;
+
+public class MeetingFactoryTest {
+  MeetingFactory mf;
+
+  @Before
+  public void setup() {
+    mf = spy(MeetingFactoryImpl.getInstance());
+  }
+
+  @Test
+  public void shouldBeAbleToGetAnInstanceOfAPastMeeting() throws InvalidMeetingException {
+    PastMeeting expected = mock(PastMeeting.class);
+    doReturn(expected).when(mf).createPastMeeting(anyInt(), anySetOf(Contact.class), any(Calendar.class), anyString());
+    Set<Contact> contactSet = new HashSet<>();
+    PastMeeting actual = mf.createPastMeeting(0, contactSet, Calendar.getInstance(), "notes");
+
+    assertThat(actual, is(equalTo(expected)));
+  }
+
+  @Test
+  public void shouldBeAbleToGetAnInstanceOfAFutureMeeting() throws InvalidMeetingException {
+    FutureMeeting expected = mock(FutureMeeting.class);
+    doReturn(expected).when(mf).createFutureMeeting(anyInt(), anySetOf(Contact.class), any(Calendar.class));
+    Set<Contact> contactSet = new HashSet<>();
+    FutureMeeting actual = mf.createFutureMeeting(0, contactSet, Calendar.getInstance());
+
+    assertThat(actual, is(equalTo(expected)));
+  }
+}
