c777ee179e3d81624051cdff93bb1e91726a358e
RyanDur <ryan.p.durling@gmail.com>
2014-02-09 23:23:10 +0000

    javadoc for the serializers

--- src/main/pij/ryan/durling/serializers/Serializers.java
+++ src/main/pij/ryan/durling/serializers/Serializers.java
@@ -4,11 +4,33 @@ import pij.ryan.durling.controllers.Contacts;
 import pij.ryan.durling.controllers.Meetings;
 
 public interface Serializers {
+
+  /**
+   * serialize meetings and contacts
+   *
+   * @param meetings
+   * @param contacts
+   */
   public void serialize(Meetings meetings, Contacts contacts);
 
+  /**
+   *
+   *
+   * @return an object array containing the deserialized meetings and contacts
+   */
   public Object[] deserialize();
 
+  /**
+   * check to see if the data file exists
+   *
+   * @return true if it does and false otherwise
+   */
   public boolean dataExists();
 
+  /**
+   * set the file name for the for persistence
+   *
+   * @param fileName
+   */
   public void setFileName(String fileName);
 }
